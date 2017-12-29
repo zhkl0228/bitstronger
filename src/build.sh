94a928618c353ec132b50d3038757afdfe5072b0
Banny <zhkl0228@qq.com>
2017-12-29 11:28:14 +0800

    Added build.sh

---
+++ src/build.sh
@@ -0,0 +1,2 @@
+g++ -DNDEBUG -O3 -Wall -pie -fPIE -obs BitStronger/ucci.cpp utility/pipe.cpp BitStronger/board.cpp  BitStronger/movement.cpp BitStronger/movemaker.cpp BitStronger/BitStronger.cpp BitStronger/hashnum.cpp BitStronger/openbook.cpp BitStronger/searcher.cpp BitStronger/evaluation.cpp
+
